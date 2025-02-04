34a0de4c2638f539f440271caa69aa2cbbd22614
eman-nishat <eman.nishat@sickkids.ca>
2025-02-03 22:11:16 -0500

    update last question

--- 02_activities/assignments/assignment2.sql
+++ 02_activities/assignments/assignment2.sql
@@ -162,19 +162,28 @@ CREATE TABLE product_units AS
 SELECT *,
 CURRENT_TIMESTAMP AS snapshot_timestamp
 FROM product
-WHERE product_qty_type = 'unit'
+WHERE product_qty_type = 'unit';
 
 /*2. Using `INSERT`, add a new row to the product_units table (with an updated timestamp). 
 This can be any product you desire (e.g. add another record for Apple Pie). */
 
-INSERT INTO product_units (
+INSERT INTO product_units (product_id, product_name, product_size, product_category_id, product_qty_type, snapshot_timestamp)
+SELECT *, CURRENT_TIMESTAMP
+FROM product
+WHERE product_id = 3;
 
 -- DELETE
 /* 1. Delete the older record for the whatever product you added. 
 
 HINT: If you don't specify a WHERE clause, you are going to have a bad time.*/
 
-
+DELETE FROM product_units
+WHERE product_id = 3
+AND snapshot_timestamp < (
+	SELECT max(snapshot_timestamp)
+	FROM product_units
+	WHERE product_id = 3
+);
 
 -- UPDATE
 /* 1.We want to add the current_quantity to the product_units table. 
@@ -193,6 +202,21 @@ Finally, make sure you have a WHERE statement to update the right row,
 	you'll need to use product_units.product_id to refer to the correct row within the product_units table. 
 When you have all of these components, you can run the update statement. */
 
+ALTER TABLE product_units
+ADD current_quantity INT;
 
-
-
+UPDATE product_units
+SET current_quantity = (
+  SELECT current_quantity 
+  FROM (
+    SELECT p.product_id, COALESCE(quantity, 0) AS current_quantity
+    FROM product_units p
+    LEFT JOIN (
+      SELECT *,
+             ROW_NUMBER() OVER (PARTITION BY vi.product_id ORDER BY market_date DESC) AS right_now
+      FROM vendor_inventory vi 
+    ) vi ON p.product_id = vi.product_id
+    WHERE right_now = 1 OR right_now IS NULL
+  ) p
+  WHERE product_units.product_id = p.product_id
+);
